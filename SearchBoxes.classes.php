858310e05715ac6312cc64cd5f8e992563fc7aa8
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2015-08-20 14:57:28 +0300

    Flip the IF for "hide in mobile" option - it was the wrong way around

--- SearchBoxes.classes.php
+++ SearchBoxes.classes.php
@@ -88,7 +88,7 @@ class SearchBoxes {
 		}
 
 		$classes = array( 'searchForm', 'hidden-print' );
-		if ( $this->mMobile === 'yes ') {
+		if ( $this->mMobile === 'no ') {
 			$classes[] = 'hidden-xs';
 		}
 		if ( $type === 'mainpage' ) {
